a5fe931bc430e4caa1d9f8028d8fafd440703631
cuihua32426 <62100614+cuihua32426@users.noreply.github.com>
2023-01-09 12:05:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,30 +13,30 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '山东',
+  CITY: '青岛',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '泞泞',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'gh_47a38a044ab3',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '发送的模板消息的模板？',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '11-19',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '泞泞', year: '1997', date: '10-20',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '泞泞', year: '1997', date: '11-19',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '恋爱纪念日', year: '2022', date: '11-02',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '看自己有没有发送成功的模板',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '黄昏',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'gh_47a38a044ab3',
     }
   ],
 
